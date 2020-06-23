412e3eb567898e191416e8b9729534d61a882ded
Ravi Jethva <rrjethva@gmail.com>
2020-06-23 12:53:27 -0400

    Updates

---
+++ precache-manifest.fce217dbffa60a421cc570fb8c4bd978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91574cff7728bb1676b433ea01e8570e",
+    "url": "/QuickChat/index.html"
+  },
+  {
+    "revision": "58f2dfd2f17654665106",
+    "url": "/QuickChat/static/css/main.b12cae02.chunk.css"
+  },
+  {
+    "revision": "fac5f2f5918e83d011d8",
+    "url": "/QuickChat/static/js/2.75d8292b.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/QuickChat/static/js/2.75d8292b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f2dfd2f17654665106",
+    "url": "/QuickChat/static/js/main.8bce0e6e.chunk.js"
+  },
+  {
+    "revision": "c6045fe5dc097fac7d12",
+    "url": "/QuickChat/static/js/runtime-main.fef37ce7.js"
+  }
+]);
